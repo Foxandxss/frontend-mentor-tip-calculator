b9ad824c16aef094a666a0c32b350d1fa2de43a2
Jesus Rodriguez <foxandxss@gmail.com>
2022-06-01 18:21:10 +0200

    solution done

--- apps/front/src/app/app.component.ts
+++ apps/front/src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'front';
+  tipAmount = 0;
+  total = 0;
+  _people = 1;
+
+  set people(value: number) {
+    this._people = value;
+    this.calculate();
+  }
+
+  get people(): number {
+    return this._people;
+  }
+
+  _bill = 0;
+
+  set bill(value: number) {
+    this._bill = value;
+    this.calculate();
+  }
+
+  get bill(): number {
+    return this._bill;
+  }
+
+  selectedTip = 5;
+
+  selectTip(amount: number) {
+    this.selectedTip = amount;
+    this.calculate();
+  }
+
+  getSelectedTip() {
+    return this.selectedTip;
+  }
+
+  calculate() {
+    this.tipAmount = (this.bill * (this.selectedTip / 100)) / this.people;
+    this.total = this.bill / this.people + this.tipAmount;
+  }
+
+  reset() {
+    this.bill = 0;
+    this.people = 1;
+    this.selectedTip = 0;
+  }
 }
